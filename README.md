065babca8f40cabd0e93a24275cc1360d6e44ebe
TejasPatel2000 <patel.tejas2000@gmail.com>
2021-01-31 20:27:31 +0000

    Add recommened songs functionality

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ a. What are at least 3 technical issues you encountered with your project? How d
     The next issue I ran into this was with deploying any changes I made. Using "git push heroku master" was not working and was throwing various errors. After more research, I found a command on stackOverflow "git push heroku HEAD:master"
     that resolved this issue for me.
     2. Ran into issue authorizing spotipy while deploying to heroku.
+    3. Some songs do not have previews for their songs
 b. What are known problems, if any, with your project? 
 c. What would you do to improve your project in the future? 
--- app.py
+++ app.py
@@ -10,23 +10,42 @@ app = Flask(__name__)
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 
+global_artist = ""
+global_track = ""
+global_genre = ""
+
 @app.route('/')
 def hello_world():
+    
     fav_artists_name = ["J Cole", "Lil Wayne", "Drake"]
     fav_artists_id   = ["6l3HvQ5sa6mXTsMTB19rO5", "55Aa2cqylxrFIXC767Z865", "3TVXtAsR1Inumwj472S9r4"]
     random_number = random.randint(0,2)
     artist = fav_artists_id[random_number]
+    name = sp.artist(artist)['name']
     
     random_number = random.randint(0,9)
-    name = sp.artist(artist)['name']
     track_name = sp.artist_top_tracks(artist, "US")['tracks'][random_number]["name"]
+    track_href=sp.artist_top_tracks(artist, "US")['tracks'][random_number]['external_urls']['spotify']
+    track_id = sp.artist_top_tracks(artist, "US")['tracks'][random_number]['id']
+    
     prev_url = sp.artist_top_tracks(artist, "US")['tracks'][random_number]["preview_url"]
     img_url = sp.artist_top_tracks(artist,"US")["tracks"][random_number]["album"]["images"][0]["url"]
+    
+    genre = sp.artist(artist)['genres'][0]
+    
+    global global_artist
+    global_artist = artist
+    
+    global global_track
+    global_track = track_id
+    
+    global global_genre
+    global_genre = genre
+    
     # print(name)
     # print(track_name)
     # print(prev_url)
     # print(img_url)
-    track_href=sp.artist_top_tracks(artist, "US")['tracks'][random_number]['external_urls']['spotify']
     return render_template(
         'index.html', 
         name = name,
@@ -37,6 +56,20 @@ def hello_world():
         )
 
     
+@app.route('/recommendations')
+def recommendations():
+   
+
+    recc= sp.recommendations(seed_artists=[global_artist], seed_genres=[global_genre], seed_tracks=[global_track], limit= 5, country="US")
+    recc_songs = {}
+    for i in range(5):
+        recc_songs[recc['tracks'][i]['name']] = recc['tracks'][i]['external_urls']['spotify']
+        
+    print(recc_songs)
+    return render_template(
+        'recommendations.html', 
+        recc_songs=recc_songs,
+        )
 
 app.run(
     port = int(os.getenv("PORT", 8080)),
